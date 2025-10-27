b99dae32c734294e3b2acb1566de8d9baad4ef6e
Sonia Kim <kimsongha12345@gmail.com>
2025-10-27 17:22:53 +0100

    tb corrected

--- tb.cpp
+++ tb.cpp
@@ -3,13 +3,9 @@
 #include <bitset>
 
 
-int main() {
-    const int N_CITY = 3;
-    double d[3][3] = {
-        {0.0, 10.0, 4.7},
-        {10.0, 0.0, 11.0},
-        {4.7, 11.0, 0.0}
-    };
-
-
+int main(){ 
+    double d[3][3] = { {0.0, 10.0, 4.7}, {10.0, 0.0, 11.0}, {4.7, 11.0, 0.0} };
+    double gamma = 0.8; double beta = 1.5; double expectation = 0.0; 
+    qaoa_kernel<3>(d, gamma, beta, &expectation); 
+    std::cout << "Expectation value = " << expectation << std::endl; return 0; 
 }
