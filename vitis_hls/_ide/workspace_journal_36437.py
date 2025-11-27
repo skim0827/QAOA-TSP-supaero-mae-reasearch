12280572d592b29c0425ae3014fff1b08d4eb0b1
KIM Songha <s.kim@pcb61-13-06.isae.fr>
2025-11-27 14:29:39 +0100

    remove gitignored files and folders

--- vitis_hls/_ide/workspace_journal_36437.py
+++
@@ -1,8 +0,0 @@
-# 2025-11-20T16:22:22.781734
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="vitis_hls")
-
-vitis.dispose()
-
--- vitis_hls/_ide/workspace_journal_39714.py
+++
@@ -1,8 +0,0 @@
-# 2025-11-20T16:33:03.877083
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="vitis_hls")
-
-vitis.dispose()
-
