0e9787ace3f5df3f948bc188a61d0c7930f2d5f9
Sonia Kim <kimsongha12345@gmail.com>
2025-12-23 16:04:03 +0100

    modified

--- HLS_codes/optimisation.cpp
+++ HLS_codes/optimisation.cpp
@@ -6,6 +6,58 @@
 #include <nlopt.hpp>
 #include <vector>
 
+
+// =========================================================
+// uart helper 
+// =========================================================
+#include <fcntl.h> 
+#include <unistd.h>
+#include <termios.h>
+
+int uart_fd;
+
+void uart_init(const char* dev = "/dev/ttyUSB1"){
+    uart_fd = open(dev, O_RDWR, O_NOCTTY); 
+
+    if (uart_fd < 0) {
+        perror("uart open failed");
+        exit(1);
+    }
+    struct termios tty{};
+    tcgetattr(uart_fd, &tty); // read current setting 
+    cfsetospeed(&tty, B115200);
+    cfsetispeed(&tty, B1152000);
+
+    tty.c_cflag |= (CLOCAL | CREAD);
+    tty.c_cflag &= ~CSIZE; 
+    tty.c_cflag |= CS8;
+    tty.c_cflag &= ~PARENB; 
+    tty.c_cflag &= ~CSTOPB;
+    
+    tcsetattr(uart_fd, TCSANOW, &tty);
+
+}
+
+// =========================================================
+// calling FPGA
+// =========================================================
+double fpga_qaoa_eval(double gamma, double beta){
+    char buf[64]; 
+    // send gamma and beta 
+    sprintf(buf, "%f %f\n", gamma, beta);
+    write(uart_fd, buf, strlen(buf));
+    // read expectation 
+    int n = read(uart_fd, buf, sizeof(buf));
+
+    if (n <= 0 ){ 
+        printf("uart read fialed\n");
+        exit(1);
+    }
+    buf[n] = 0; // -- string terminator '\n'
+
+    return atof(buf); // -- text to double
+}
+
 static double d[3][3] = {
     {0.0, 10.0, 4.7},
     {10.0, 0.0, 11.0},
@@ -30,25 +82,22 @@ uint32_t brute_force(const double d[N_CITY][N_CITY], double &min_energy_out) {
     return best_state;
 }
 
-// ------------------- objective function for NLopt -------------------
+// =========================================================
+// NLopt objective
+// =========================================================
 double objective_function(const std::vector<double>& x, std::vector<double>& grad, void* data) {
     (void)grad; (void)data;
-
-    static const int P = 3 ;
-    double expectation = 0.0;
-    double gamma[P] = { x[0], x[2], x[4] };
-    double beta[P]  = { x[1], x[3], x[5] };
-
-    uint32_t dummy = 0;
     
-    expectation = qaoa_kernel(d, gamma, beta, false, &dummy);
+    // expectation = qaoa_kernel(d, gamma, beta, false, &dummy);
+    double expectation = fpga_qaoa_eval(x[0], x[1]);
+
     return expectation;
 }
 
 // ------------------- Debug wrapper -------------------
 void qaoa_kernel_debug(const double d[3][3],
-                       const double gamma[3],
-                       const double beta[3],
+                       const double gamma[1],
+                       const double beta[1],
                        double* expectation_out,
                        uint32_t* best_state_out)
 {
@@ -67,70 +116,31 @@ void qaoa_kernel_debug(const double d[3][3],
     std::cout << "Best bitstring (QAOA p=2)  : "
               << std::bitset<9>(best_state) << std::endl;}
 
-
-// ------------------- Main -------------------
+// =========================================================
+// main 
+// =========================================================
 int main() {
-    const int N_CITY = 3;
-    double d[3][3] = {
-        {0.0, 10.0, 4.7},
-        {10.0, 0.0, 11.0},
-        {4.7, 11.0, 0.0}
-    };
 
-    // initial guess for γ1, β1, γ2, β2
-    double gamma1 = 0.5;
-    double beta1  = 0.5;
-    double gamma2 = 0.3;
-    double beta2  = 0.3;
-    double gamma3 = 0.3;
-    double beta3  = 0.3;
-    
+    uart_init();
 
     // --- Set up NLopt (Nelder–Mead) ---
-    nlopt::opt opt(nlopt::LN_NELDERMEAD, 6); // 4 parameters to optimise
-    opt.set_lower_bounds({0.0, 0.0, 0.0, 0.0, 0.0, 0.0});
-    opt.set_upper_bounds({M_PI, M_PI, M_PI, M_PI, M_PI, M_PI});
+    nlopt::opt opt(nlopt::LN_NELDERMEAD, 2); // parameters to optimise
+    opt.set_lower_bounds({0.0, 0.0});
+    opt.set_upper_bounds({M_PI, M_PI});
     opt.set_min_objective(objective_function, nullptr);
     opt.set_xtol_rel(1e-4);
     
     // --- Run optimizer ---
-    std::vector<double> x = {gamma1, beta1, gamma2, beta2, gamma3, beta3};
+    std::vector<double> x = {0.5, 0.5};
     double min_expectation ;
     nlopt::result result = opt.optimize(x, min_expectation);
 
-    // --- Final evaluation ---
-    double final_expectation = 0.0;
-    uint32_t best_state = -1;
+    std::cout << "Optimisation finished\n";
+    std::cout << "gamma = " << x[0] << "\n";
+    std::cout << "beta  = " << x[1] << "\n";
+    std::cout << "expectation = " << min_expectation << "\n";    
 
-    double gamma[3] = { x[0], x[2], x[4] };
-    double beta[3]  = { x[1], x[3], x[5] };
-    qaoa_kernel_debug(d, gamma, beta, &final_expectation, &best_state);
-    std::cout << "\nFinal Results (p=2 QAOA):" << std::endl;
-    std::cout << "  γ1 = " << gamma[0] << ", β1 = " << beta[0] << std::endl;
-    std::cout << "  γ2 = " << gamma[1] << ", β2 = " << beta[1] << std::endl;
-    std::cout << "  γ3 = " << gamma[2] << ", β3 = " << beta[2] << std::endl;
-
-    std::cout << "  Expectation = " << final_expectation << std::endl;
-    std::cout << "  Best bitstring (9 bits): "
-              << std::bitset<9>(best_state) << std::endl;
-
-    
-    // optional: compare with brute-force
-    double min_energy;
-    uint32_t brute_best = brute_force<3>(d, min_energy);
-    std::cout << "Brute-force optimal energy: " << min_energy << std::endl;
-    std::cout << "Brute-force best bitstring: "
-              << std::bitset<9>(brute_best) << std::endl;
-
-    
-    // ---- debug ----
-    // double E_brute = costHamiltonian<3>(0b001010100, d);
-    // double E_fpga  = costHamiltonian<3>(0b010010010, d);
-
-    // std::cout << "E(brute)=" << E_brute << std::endl;
-    // std::cout << "E(fpga) =" << E_fpga  << std::endl;
-
-
+    close(uart_fd);
     return 0;
 }
 
