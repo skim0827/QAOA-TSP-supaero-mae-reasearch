91b7d3b69f930a2178709c0626063b2663b76fb1
skim0827 <kimsongha12345@gmail.com>
2025-12-28 09:41:24 +0100

    Update main.c
    trying to make PC -- ARM -- FPGA -- ARM -- PC cycle

--- vitis_app/src/main.c
+++ vitis_app/src/main.c
@@ -1,5 +1,6 @@
 #include "xparameters.h"
 #define QAOA_BASE XPAR_QAOA_KERNEL_0_BASEADDR
+volatile uint32_t *qaoa = (uint32_t*)QAOA_BASE;
 #include "xuartps.h"
 #include <stdio.h>
 #include <stdint.h>
@@ -16,21 +17,20 @@ static inline uint32_t float_to_fixed(float x){
 static inline float fixed_to_float(uint32_t x){
     return ((float)x) / (1 << 20);
 }
+// ---------------------------
+// UART 
+// ---------------------------
 
-int main() {
-    // ---------------------------
-    // UART 
-    // ---------------------------
-    XUartPs uart;
+XUartPs uart;
+void uart_init(){ 
     XUartPs_Config *cfg;
     cfg = XUartPs_LookupConfig(XPAR_DEVICE_ID);
     XUartPs_CfgInitialize(&uart, cfg, cfg->BaseAddress);
     XUartPs_SetBaudRate(&uart, 115200);
+}
 
-
-    char buf[64];
+void uart_recv_PC(char *buf){ 
     int i = 0;
-
     // PC -> ARM wait until newline
     while (1) {
         if (XUartPs_IsReceiveData(uart.Config.BaseAddress)) {
@@ -39,72 +39,60 @@ int main() {
             if (buf[i-1] == '\n') break;
         }
     }
-    buf[i] = 0;
+    buf[i] = 0 ; 
+}
 
+void run_fpga(){
+    qaoa[0] = 1; // ap_start 
+    while((qaoa[0] & 0x2) == 0)); // wait until ap_done
+}
+
+void uart_send_to_pc(float v){
+    char buf[32]; 
+    sprintf(buf, "%f\n", v); 
+    for (int i = 0; buf[i]; i++){ 
+        XUartPs_Write_Reg(uart.Config.BaseAdress, XUARTPS_FIFO_OFFSET, buf[i]); 
+    }    
+}
+
+int main() {
+
+    uart_init();
+
+    char buf[64];
     float gamma, beta;
-    sscanf(buf, "%f %f", &gamma, &beta);
 
+    while(1){
 
+    // 1. receive from PC
+        uart_recv_PC(buf); 
+        sscanf(buf, "%f %f", &gamma, &beta)
 
+    // 2. write to fpga
+        qaoa[6] = float_to_fixed(gamma);
+        qaoa[8] = float_to_fixed(beta);    
+        
+    // 3. run fpga 
+        run_fpga();
 
-    volatile uint32_t *reg = (uint32_t*)QAOA_BASE;
+    // 4. read result
+        float expectation = fixed_to_float(qaoa[4]);
 
-    // ---------------------------
-    // 1. Write matrix d[3][3]
-    // ---------------------------
-    float d[3][3] = {
-        {0.0, 10.0, 4.7},
-        {10.0, 0.0, 11.0},
-        {4.7, 11.0, 0.0}
-    };
-
-    int idx = 16;   // reg[0] = control register
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            reg[idx++] = float_to_fixed(d[i][j]);
-        }
+    // 5. send back to PC 
+        uart_send_to_pc(expectation);
     }
+    
+    // ---------------------------
+    // time measurement 
+    // ---------------------------
+    // XTime t0, t1;
+    // XTime_GetTime(&t0);
+    // reg[0] = 1;
+    // while ((reg[0] & 0x2) == 0);   // wait ap_done
+    // XTime_GetTime(&t1);
+    // double fpga_ms = (double)(t1 - t0) / (COUNTS_PER_SECOND / 1000.0);
+    // printf("FPGA time = %f ms\n", fpga_ms);
 
-    // ---------------------------
-    // 2. Write gamma, beta
-    // ---------------------------
-    reg[6] = float_to_fixed(gamma);
-    reg[8] = float_to_fixed(beta);    
-
-    // ---------------------------
-    // 3. get_best_state = 1
-    // ---------------------------
-    reg[10] = 1;
-
-    // ---------------------------
-    // 4. Start accelerator ap_start = 1
-    // ---------------------------
-    XTime t0, t1;
-    XTime_GetTime(&t0);
-    reg[0] = 1;
-    while ((reg[0] & 0x2) == 0);   // wait ap_done
-    XTime_GetTime(&t1);
-    double fpga_ms = (double)(t1 - t0) / (COUNTS_PER_SECOND / 1000.0);
-    printf("FPGA time = %f ms\n", fpga_ms);
-
-    // ---------------------------
-    // 5. Read results
-    // ---------------------------
-    uint32_t best_state_raw = reg[12];
-    uint32_t expect_raw     = reg[4];
-    float expectation = fixed_to_float(expect_raw);
-
-    // ARM -> PC 
-    char out[64];
-    sprintf(out, "%f\n", expectation);
-    for (int i = 0; out[i] != 0; i++) {
-    XUartPs_WriteReg(uart.Config.BaseAddress,
-                     XUARTPS_FIFO_OFFSET,
-                     out[i]);
-    }
-
-    printf("Best state  = %u\n", best_state_raw);
-    printf("Expectation = %f\n", expectation);
 
     return 0;
 }
